ad7b21e87a382c287e67d2b40c4dcdc2be8575e0
Johan Abildskov <randomsort@gmail.com>
2017-06-12 19:12:06 +0200

    bit more work

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -7,7 +9,7 @@ def index():
     user = {'nickname': 'Sofus'}
     posts = [
             {
-                'author': {'nickname': 'Thierry'},
+                'author': {'nickname': 'Thierry Lacour'},
                 'body' : 'Random Pokémon Rant'
                 },
             {
@@ -20,3 +22,13 @@ def index():
             title='CoDeAcademy',
             user=user,
             posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' % (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html',
+                title='Sign In',
+                form= form)
