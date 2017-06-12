ca6c565445b191bc4cacaf26ecaccb7b74f83bce
Johan Abildskov <randomsort@gmail.com>
2017-06-12 19:50:38 +0200

    more work

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'Iwrotethisinvimsoitcanbeassumedtobepseudorandom'
+
+OPENID_PROVIDERS = [
+        {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'}
+        ]
