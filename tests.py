2fade87312077a807c857abedeb63c19c271b60b
Johan Abildskov <jabildskov@grundfos.com>
2017-06-14 10:31:47 +0200

    More useful test

--- tests.py
+++ tests.py
@@ -1,10 +1,10 @@
 #!flask/bin/python
 import os
 import unittest
-
+from datetime import datetime
 from config import basedir
 from app import app, db
-from app.models import User
+from app.models import Post
 
 class TestCase(unittest.TestCase):
     def setUp(self):
@@ -20,6 +20,14 @@ class TestCase(unittest.TestCase):
 
     def test_silly(self):
         assert True
+    
+    def test_add_post_is_added(self):
+        post = Post(title='test1', user_name='test1', body='test1', timestamp=datetime.utcnow())
+        db.session.add(post)
+        db.session.commit()
+        print(len(Post.query.all()))
+        number_of_posts = len(Post.query.all())
+        self.assertEqual(number_of_posts,1)
 
 if __name__ == '__main__':
     unittest.main()
