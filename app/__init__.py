5c0a6b6d581a0b3f3126b9596acc89400922012a
Johan Abildskov <jabildskov@grundfos.com>
2017-06-14 11:00:36 +0200

    comment __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+# Instantiate our app and load configuration
 app = Flask(__name__)
 app.config.from_object('config')
+
 db = SQLAlchemy(app)
 
 from app import views, models
