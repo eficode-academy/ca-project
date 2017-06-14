611913d225fdd4ac1850e46a6fc70df06a9085e6
Johan Abildskov <jabildskov@grundfos.com>
2017-06-14 10:58:09 +0200

    Document and cleanup config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# Setup out SQLITE database connection
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+# Do cross site request forgery protection
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'Iwrotethisinvimsoitcanbeassumedtobepseudorandom'
-
-OPENID_PROVIDERS = [
-        {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'}
-        ]
