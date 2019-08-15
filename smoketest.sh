637a2c5ef30ceecd03f38605417571f82233290a
NJohansen <32328015+NJohansen@users.noreply.github.com>
2019-08-15 16:08:14 +0200

    changed workflow

--- smoketest.sh
+++ smoketest.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
-
+docker container stop $(docker ps -a -q)
 docker container prune -f
 docker pull grameaway/codechan:latest
 docker container run -d -p 5000:5000 --rm --name codechan grameaway/codechan
 
 touch response.txt
-sleep 3
+sleep 10
 curl -sL -w "%{http_code}" -I "$1:5000" -o /dev/null > response.txt
 if grep -q 200 "response.txt"; then
-    docker container stop $(docker ps -a -q)
     echo "Succes!"
     exit 0
 fi
-docker container stop $(docker ps -a -q)
 echo "Failed!"
 exit N
