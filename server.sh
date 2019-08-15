46ad7983ee95b71a2ecc881a1646e409dec35136
Frederik Kvartborg Albertsen <hello@kvartborg.net>
2019-08-15 14:33:05 +0200

    Better versioning (#38)
    * Add better versioning
    
    * test
    
    * fix yaml
    
    * Fix docker

--- server.sh
+++ server.sh
@@ -3,5 +3,5 @@
 docker container stop $(docker ps -a -q)
 docker container rm $(docker ps -a -q)
 
-docker image rm dueruen/ca-project:$TARGET
-docker container run --name=$TARGET -d -p 80:5000 -v "$(pwd)"/app.db:/app/app.db dueruen/ca-project:$TARGET
+docker image rm dueruen/ca-project:$1
+docker container run --name=$TARGET -d -p 80:5000 -v "$(pwd)"/app.db:/app/app.db dueruen/ca-project:$1
