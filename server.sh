1cdd3925592e5daf72ac9d5f25b420b60c1d5f33
Frederik Kvartborg Albertsen <hello@kvartborg.net>
2019-08-15 14:02:00 +0200

    Fix command (#35)
    * Fix server execution
    
    * Oneline
    
    * add env over ssh

--- server.sh
+++ server.sh
@@ -4,9 +4,4 @@ docker container stop $(docker ps -a -q)
 docker container rm $(docker ps -a -q)
 
 docker image rm dueruen/ca-project:$TARGET
-docker container run \
-  --name=$TARGET \
-  -d \
-  -p 80:5000 \
-  -v "$(pwd)"/app.db:/app/app.db \
-  dueruen/ca-project:$TARGET
+docker container run --name=$TARGET -d -p 80:5000 -v "$(pwd)"/app.db:/app/app.db dueruen/ca-project:$TARGET
