4662fbbd1aa063d70c08dc7b9d22427c98b4cca7
Frederik Kvartborg Albertsen <hello@kvartborg.net>
2019-08-15 11:19:13 +0200

    Fix script issues (#23)

--- server.sh
+++ server.sh
@@ -4,4 +4,4 @@ docker container stop $(docker ps -a -q)
 docker container rm $(docker ps -a -q)
 
 docker image rm dueruen/ca-project:latest
-docker container run dueruen/ca-project:lates -d
+docker container run --name=server -d -p 80:5000 -v app.db:/app/ dueruen/ca-project:latest
