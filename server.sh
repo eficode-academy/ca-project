6f4d17734bded6bceb126b1012b36fac433eed9a
Victor Gram Thomsen <victor_gram@ymail.com>
2019-08-15 12:13:19 +0200

    this will definitely work

--- server.sh
+++ server.sh
@@ -1,3 +1,4 @@
 docker pull grameaway/codechan:latest   
-docker container stop $(docker ps -a -q) 
+docker container stop $(docker ps -a -q)
+docker image rm grameaway/codechan 
 docker run -p 5000:5000 --rm --name codechan grameaway/codechan
