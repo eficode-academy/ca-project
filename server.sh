6ed40a2d16c26b26ed27f4da9ef868ee53f6c117
NJohansen <32328015+NJohansen@users.noreply.github.com>
2019-08-15 13:01:23 +0200

    container

--- server.sh
+++ server.sh
@@ -1,3 +1,3 @@
 docker container stop $(docker ps -a -q)
 docker image rm grameaway/codechan
-docker run -p 5000:5000 --rm --name codechan grameaway/codechan:latest
+docker container run -p 5000:5000 --rm --name codechan grameaway/codechan
