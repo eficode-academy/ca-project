bf14aa03e95d58f58283349bce7299d75319ba4d
kvartborg <hello@kvartborg.net>
2019-08-15 12:04:53 +0200

    db should be persistent

--- server.sh
+++ server.sh
@@ -3,5 +3,10 @@
 docker container stop $(docker ps -a -q)
 docker container rm $(docker ps -a -q)
 
-docker image rm dueruen/ca-project:latest
-docker container run --name=server -d -p 80:5000 -v app.db:/app/ dueruen/ca-project:latest
+docker image rm dueruen/ca-project
+docker container run \
+  --name=server \
+  -d \
+  -p 80:5000 \
+  -v \
+  "$(pwd)"/app.db:/app/app.db dueruen/ca-project
