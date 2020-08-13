16886f3903559f467c738fde2748bcd3fd3e0f83
simonmdsn <43004140+simonmdsn@users.noreply.github.com>
2020-08-13 10:10:42 +0200

    shell script fix

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::4}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-docker build -t "${DockerRepo}flaskproject:latest" -t "${DockerRepo}flaskproject:1.0-$Tag" app/
+docker build -t "${DockerRepo}flaskproject:latest" -t "${DockerRepo}flaskproject:1.0-$Tag"
