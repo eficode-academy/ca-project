d58e1dcb9db0d55966a5c03b22b99f0e94d5bbaf
MortenAbra <sezure@gmail.com>
2020-08-13 14:12:42 +0200

    Removed sudo

--- build_zip.sh
+++ build_zip.sh
@@ -1,2 +1,2 @@
 apt-get install -y zip unzip
-sudo zip -r ca-build.zip ./ -x '*.git*' '*.md*' '*Dockerfile*' '*Jenkinsfile*' '*.sh*' '*.png*'
+zip -r ca-build.zip ./ -x '*.git*' '*.md*' '*Dockerfile*' '*Jenkinsfile*' '*.sh*' '*.png*'
