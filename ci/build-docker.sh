14deabc01ac94cf5163974bf42f393ddacb48880
simonmdsn <43004140+simonmdsn@users.noreply.github.com>
2020-08-13 09:47:47 +0200

    Jenkinsfile

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,4 +1,6 @@
+echo "Hello world"
 #!/bin/bash
+echo "Hello world 2"
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::4}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
 docker build -t "${DockerRepo}flaskproject:latest" -t "${DockerRepo}flaskproject:1.0-$Tag" app/
